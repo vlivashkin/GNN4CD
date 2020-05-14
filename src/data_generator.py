6f4a87c823cbdcb421ac7bd609571fc894a3a2fc
Vladimir Ivashkin <illusionww@gmail.com>
2020-05-14 11:12:26 +0300

    cleanup

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,36 +1,17 @@
-import numpy as np
-import os
-import time
 import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-import networkx
-
-import unicodedata
-import string
-import re
-import random
-import argparse
-
+import numpy as np
 import torch
-import torch.nn as nn
-from torch.nn import init
 from torch.autograd import Variable
-from torch import optim
-import torch.nn.functional as F
-from load import get_Pm, get_Pd, get_W_lg
+
+matplotlib.use('Agg')
 
 
 class Generator(object):
-    def __init__(self, N_train=50, N_test=100, generative_model='SBM_multiclass', p_SBM=0.8, q_SBM=0.2, n_classes=2, path_dataset='', num_examples_train=100, num_examples_test=10):
+    def __init__(self, N_train=50, N_test=100, generative_model='SBM_multiclass', p_SBM=0.8, q_SBM=0.2, n_classes=2,
+                 path_dataset='', num_examples_train=100, num_examples_test=10):
         self.N_train = N_train
         self.N_test = N_test
-        # self.generative_model = 'ErdosRenyi'
         self.generative_model = generative_model
-        # self.edge_density = 0.2
-        # self.random_noise = False
-        # self.noise = 0.03
-        # self.noise_model = 2
         self.p_SBM = p_SBM
         self.q_SBM = q_SBM
         self.n_classes = n_classes
@@ -43,15 +24,12 @@ class Generator(object):
     def SBM(self, p, q, N):
         W = np.zeros((N, N))
 
-        p_prime = 1 - np.sqrt(1 - p)
-        q_prime = 1 - np.sqrt(1 - q)
-
         n = N // 2
 
         W[:n, :n] = np.random.binomial(1, p, (n, n))
-        W[n:, n:] = np.random.binomial(1, p, (N-n, N-n))
-        W[:n, n:] = np.random.binomial(1, q, (n, N-n))
-        W[n:, :n] = np.random.binomial(1, q, (N-n, n))
+        W[n:, n:] = np.random.binomial(1, p, (N - n, N - n))
+        W[:n, n:] = np.random.binomial(1, q, (n, N - n))
+        W[n:, :n] = np.random.binomial(1, q, (N - n, n))
         W = W * (np.ones(N) - np.eye(N))
         W = np.maximum(W, W.transpose())
 
@@ -72,9 +50,7 @@ class Generator(object):
         n = N // n_classes
 
         for i in range(n_classes):
-            prob_mat[i * n : (i+1) * n, i * n : (i+1) * n] = p_prime
-
-        # print ('prob mat', prob_mat)
+            prob_mat[i * n: (i + 1) * n, i * n: (i + 1) * n] = p_prime
 
         W = np.random.rand(N, N) < prob_mat
         W = W.astype(int)
@@ -83,142 +59,21 @@ class Generator(object):
         W = np.maximum(W, W.transpose())
 
         perm = torch.randperm(N).numpy()
-        # blockA = perm < n
-        # labels = blockA * 2 - 1
         labels = (perm // n)
 
         W_permed = W[perm]
         W_permed = W_permed[:, perm]
         return W_permed, labels
 
-    def ErdosRenyi(self, p, N):
-        W = np.zeros((N, N))
-        for i in range(0, N - 1):
-            for j in range(i + 1, N):
-                add_edge = (np.random.uniform(0, 1) < p)
-                if add_edge:
-                    W[i, j] = 1
-                W[j, i] = W[i, j]
-        return W
-
-    def ErdosRenyi_netx(self, p, N):
-        g = networkx.erdos_renyi_graph(N, p)
-        W = networkx.adjacency_matrix(g).todense().astype(float)
-        W = np.array(W)
-        return W
-
-    def RegularGraph_netx(self, p, N):
-        """ Generate random regular graph """
-        d = p * N
-        d = int(d)
-        g = networkx.random_regular_graph(d, N)
-        W = networkx.adjacency_matrix(g).todense().astype(float)
-        W = np.array(W)
-        return W
-
-    def create_dataset(self, directory, is_training):
-        if (self.generative_model == 'SBM_multiclass'):
-            if not os.path.exists(directory):
-                os.mkdir(directory)
-            if is_training:
-                graph_size = self.N_train
-                num_graphs = self.num_examples_train
-            else:
-                graph_size = self.N_test
-                num_graphs = self.num_examples_test
-            dataset = []
-            for i in range(num_graphs):
-                W, labels = self.SBM_multiclass(self.p_SBM, self.q_SBM, graph_size, self.n_classes)
-                Pm = get_Pm(W)
-                Pd = get_Pd(W)
-                NB = get_W_lg(W)
-                example = {}
-                example['W'] = W
-                example['labels'] = labels
-                # example['Pm'] = Pm
-                # example['Pd'] = Pd
-                # example['NB'] = NB
-                dataset.append(example)
-            if is_training:
-                print ('Saving the training dataset')
-            else:
-                print ('Saving the testing dataset')
-            np.save(directory + '/dataset.npy', dataset)
-            if is_training:
-                self.data_train = dataset
-            else:
-                self.data_test = dataset
-        else:
-            raise ValueError('Generative model {} not supported'.format(self.generative_model))
-
-
-
-    def prepare_data(self):
-        train_directory = self.generative_model + '_nc' + str(self.n_classes) + '_p' + str(self.p_SBM) + '_q' + str(self.q_SBM) + '_gstr' + str(self.N_train) + '_numtr' + str(self.num_examples_train)
-        
-        train_path = os.path.join(self.path_dataset, train_directory)
-        if os.path.exists(train_path + '/dataset.npy'):
-            print('Reading training dataset at {}'.format(train_path))
-            # self.data_train = self.load_from_directory(train_path)
-            self.data_train = np.load(train_path + '/dataset.npy')
-        else:
-            print('Creating training dataset.')
-            self.create_dataset(train_path, is_training=True)
-            # print('Saving training datatset at {}'.format(train_path))
-            # np.save(path, self.data_train)
-        # load test dataset
-        test_directory = self.generative_model + '_nc' + str(self.n_classes) + '_p' + str(self.p_SBM) + '_q' + str(self.q_SBM) + '_gste' + str(self.N_test) + '_numte' + str(self.num_examples_test)
-        test_path = os.path.join(self.path_dataset, test_directory)
-        if os.path.exists(test_path + '/dataset.npy'):
-            print('Reading testing dataset at {}'.format(test_path))
-            # self.data_test = self.load_from_directory(test_path)
-            self.data_test = np.load(test_path + '/dataset.npy')
-        else:
-            print('Creating testing dataset.')
-            self.create_dataset(test_path, is_training=False)
-            # print('Saving testing datatset at {}'.format(test_path))
-            # np.save(open(path, 'wb'), self.data_test)
-
-    def sample_single(self, i, is_training=True):
-        if is_training:
-            dataset = self.data_train
-        else:
-            dataset = self.data_test
-        example = dataset[i]
-        if (self.generative_model == 'SBM_multiclass'):
-            W_np = example['W']
-            labels = np.expand_dims(example['labels'], 0)
-            labels_var = Variable(torch.from_numpy(labels), volatile=not is_training)
-            return W_np, labels_var 
-
     def sample_otf_single(self, is_training=True, cuda=True):
-        if is_training:
-            N = self.N_train
-        else:
-            N = self.N_test
+        N = self.N_train if is_training else self.N_test
         if self.generative_model == 'SBM':
             W, labels = self.SBM(self.p_SBM, self.q_SBM, N)
         elif self.generative_model == 'SBM_multiclass':
             W, labels = self.SBM_multiclass(self.p_SBM, self.q_SBM, N, self.n_classes)
         else:
             raise ValueError('Generative model {} not supported'.format(self.generative_model))
-        # if self.random_noise:
-        #     self.noise = np.random.uniform(0.000, 0.050, 1)
-        # if self.noise_model == 1:
-        #     # use noise model from [arxiv 1602.04181], eq (3.8)
-        #     noise = self.ErdosRenyi(self.noise, self.N_train)
-        #     W_noise = W*(1-noise) + (1-W)*noise
-        # elif self.noise_model == 2:
-        #     # use noise model from [arxiv 1602.04181], eq (3.9)
-        #     pe1 = self.noise
-        #     pe2 = (self.edge_density*self.noise)/(1.0-self.edge_density)
-        #     noise1 = self.ErdosRenyi_netx(pe1, self.N_train)
-        #     noise2 = self.ErdosRenyi_netx(pe2, self.N_train)
-        #     W_noise = W*(1-noise1) + (1-W)*noise2
-        # else:
-        #     raise ValueError('Noise model {} not implemented'.format(self.noise_model))
         labels = np.expand_dims(labels, 0)
         labels = Variable(torch.from_numpy(labels), volatile=not is_training)
         W = np.expand_dims(W, 0)
         return W, labels
-
--- src/main_lgnn.py
+++ src/main_lgnn.py
@@ -1,47 +1,32 @@
-import numpy as np
-import os
-from data_generator import Generator
-from load import get_lg_inputs
-from models import lGNN_multiclass
-import time
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-
-import unicodedata
-import string
-import re
-import random
 import argparse
+import os
+import time
 
+import matplotlib
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn import init
-from torch.autograd import Variable
-from torch import optim
-import torch.nn.functional as F
-from losses import compute_loss_multiclass, compute_accuracy_multiclass
 
+from data_generator import Generator
+from load import get_lg_inputs
+from losses import compute_loss_multiclass, compute_accuracy_multiclass
+from models import lGNN_multiclass
+
+matplotlib.use('Agg')
 parser = argparse.ArgumentParser()
 
 ###############################################################################
 #                             General Settings                                #
 ###############################################################################
-parser.add_argument('--num_examples_train', nargs='?', const=1, type=int,
-                    default=int(6000))
-parser.add_argument('--num_examples_test', nargs='?', const=1, type=int,
-                    default=int(1000))
-parser.add_argument('--edge_density', nargs='?', const=1, type=float,
-                    default=0.2)
-parser.add_argument('--p_SBM', nargs='?', const=1, type=float,
-                    default=0.8)
-parser.add_argument('--q_SBM', nargs='?', const=1, type=float,
-                    default=0.2)
+parser.add_argument('--num_examples_train', nargs='?', const=1, type=int, default=int(6000))
+parser.add_argument('--num_examples_test', nargs='?', const=1, type=int, default=int(1000))
+parser.add_argument('--edge_density', nargs='?', const=1, type=float, default=0.2)
+parser.add_argument('--p_SBM', nargs='?', const=1, type=float, default=0.8)
+parser.add_argument('--q_SBM', nargs='?', const=1, type=float, default=0.2)
 parser.add_argument('--random_noise', action='store_true')
 parser.add_argument('--noise', nargs='?', const=1, type=float, default=0.03)
 parser.add_argument('--noise_model', nargs='?', const=1, type=int, default=2)
-parser.add_argument('--generative_model', nargs='?', const=1, type=str,
-                    default='ErdosRenyi')
+parser.add_argument('--generative_model', nargs='?', const=1, type=str, default='ErdosRenyi')
 parser.add_argument('--batch_size', nargs='?', const=1, type=int, default=1)
 parser.add_argument('--mode', nargs='?', const=1, type=str, default='train')
 parser.add_argument('--path_dataset', nargs='?', const=1, type=str, default='')
@@ -88,28 +73,25 @@ template2 = '{:<10} {:<10.5f} {:<10.5f} {:<15} {:<10} {:<10} {:<10.3f} \n'
 template3 = '{:<10} {:<10} {:<10} '
 template4 = '{:<10} {:<10.5f} {:<10.5f} \n'
 
-def train_mcd_single(gnn, optimizer, gen, n_classes, it):
+
+def train_mcd_single(gnn: lGNN_multiclass, optimizer, gen: Generator, n_classes, it):
     start = time.time()
     W, labels = gen.sample_otf_single(is_training=True, cuda=torch.cuda.is_available())
     labels = labels.type(dtype_l)
 
-    print ('Num of edges: ', np.sum(W))
+    print('Num of edges: ', np.sum(W))
 
     if (args.generative_model == 'SBM_multiclass') and (args.n_classes == 2):
-        labels = (labels + 1)/2
+        labels = (labels + 1) / 2
 
     WW, x, WW_lg, y, P = get_lg_inputs(W, args.J)
 
-    # print ('WW', WW.shape)
-    # print ('WW_lg', WW_lg.shape)
-
-    if (torch.cuda.is_available()):
+    if torch.cuda.is_available():
         WW.cuda()
         x.cuda()
         WW_lg.cuda()
         y.cuda()
         P.cuda()
-    # print ('input', input)
     pred = gnn(WW.type(dtype), x.type(dtype), WW_lg.type(dtype), y.type(dtype), P.type(dtype))
 
     loss = compute_loss_multiclass(pred, labels, n_classes)
@@ -122,15 +104,13 @@ def train_mcd_single(gnn, optimizer, gen, n_classes, it):
 
     elapsed = time.time() - start
 
-    if(torch.cuda.is_available()):
+    if torch.cuda.is_available():
         loss_value = float(loss.data.cpu().numpy())
     else:
         loss_value = float(loss.data.numpy())
 
-    info = ['iter', 'avg loss', 'avg acc', 'edge_density',
-            'noise', 'model', 'elapsed']
-    out = [it, loss_value, acc, args.edge_density,
-           args.noise, 'LGNN', elapsed]
+    info = ['iter', 'avg loss', 'avg acc', 'edge_density', 'noise', 'model', 'elapsed']
+    out = [it, loss_value, acc, args.edge_density, args.noise, 'LGNN', elapsed]
     print(template1.format(*info))
     print(template2.format(*out))
 
@@ -142,14 +122,13 @@ def train_mcd_single(gnn, optimizer, gen, n_classes, it):
 
     return loss_value, acc
 
-def train(gnn, gen, n_classes=args.n_classes, iters=args.num_examples_train):
+
+def train(gnn: lGNN_multiclass, gen: Generator, n_classes=args.n_classes, iters=args.num_examples_train):
     gnn.train()
     optimizer = torch.optim.Adamax(gnn.parameters(), lr=args.lr)
     loss_lst = np.zeros([iters])
     acc_lst = np.zeros([iters])
     for it in range(iters):
-        # W, labels = gen.sample_otf_single(is_training=True, cuda=torch.cuda.is_available())
-        # WW, x, WW_lg, y, P = get_lg_inputs(W, args.J)
         # print ("Num of edges: ", np.sum(W))
         loss_single, acc_single = train_mcd_single(gnn, optimizer, gen, n_classes, it)
         loss_lst[it] = loss_single
@@ -158,34 +137,31 @@ def train(gnn, gen, n_classes=args.n_classes, iters=args.num_examples_train):
 
         if (it % 100 == 0) and (it >= 100):
             # print ('Testing at check_pt begins')
-            print ('Check_pt at iteration ' + str(it) + ' :')
-            print ('Avg train loss', np.mean(loss_lst[it-100:it]))
-            print ('Avg train acc', np.mean(acc_lst[it-100:it]))
-            print ('Std train acc', np.std(acc_lst[it-100:it]))
+            print('Check_pt at iteration ' + str(it) + ' :')
+            print('Avg train loss', np.mean(loss_lst[it - 100:it]))
+            print('Avg train acc', np.mean(acc_lst[it - 100:it]))
+            print('Std train acc', np.std(acc_lst[it - 100:it]))
 
-    print ('Final avg train loss', np.mean(loss_lst))
-    print ('Final avg train acc', np.mean(acc_lst))
-    print ('Final std train acc', np.std(acc_lst))
+    print('Final avg train loss', np.mean(loss_lst))
+    print('Final avg train acc', np.mean(acc_lst))
+    print('Final std train acc', np.std(acc_lst))
 
-def test_mcd_single(gnn, gen, n_classes, it):
 
+def test_mcd_single(gnn: lGNN_multiclass, gen: Generator, n_classes, it):
     start = time.time()
     W, labels = gen.sample_otf_single(is_training=False, cuda=torch.cuda.is_available())
     labels = labels.type(dtype_l)
     if (args.generative_model == 'SBM_multiclass') and (args.n_classes == 2):
-        labels = (labels + 1)/2
+        labels = (labels + 1) / 2
     WW, x, WW_lg, y, P = get_lg_inputs(W, args.J)
 
-    # print ('WW', WW.shape)
-    # print ('WW_lg', WW_lg.shape)
-
-    if (torch.cuda.is_available()):
+    if torch.cuda.is_available():
         WW.cuda()
         x.cuda()
         WW_lg.cuda()
         y.cuda()
         P.cuda()
-    # print ('input', input)
+
     pred_single = gnn(WW.type(dtype), x.type(dtype), WW_lg.type(dtype), y.type(dtype), P.type(dtype))
     labels_single = labels
 
@@ -193,17 +169,8 @@ def test_mcd_single(gnn, gen, n_classes, it):
     acc_test = compute_accuracy_multiclass(pred_single, labels_single, n_classes)
 
     elapsed = time.time() - start
-    # if (it % args.print_freq == 0):
-    #     info = ['it', 'avg loss', 'avg acc', 'edge_density',
-    #             'noise', 'model', 'elapsed']
-    #     out = [it, loss_test, acc_test, args.edge_density,
-    #            args.noise, 'lGNN', elapsed]
-    #     print(template1.format(*info))
-    #     print(template2.format(*out))
 
-    elapsed = time.time() - start
-
-    if(torch.cuda.is_available()):
+    if torch.cuda.is_available():
         loss_value = float(loss_test.data.cpu().numpy())
     else:
         loss_value = float(loss_test.data.numpy())
@@ -223,28 +190,26 @@ def test_mcd_single(gnn, gen, n_classes, it):
 
     return loss_value, acc_test
 
-def test(gnn, gen, n_classes, iters=args.num_examples_test):
+
+def test(gnn: lGNN_multiclass, gen: Generator, n_classes, iters=args.num_examples_test):
     gnn.train()
     loss_lst = np.zeros([iters])
     acc_lst = np.zeros([iters])
     for it in range(iters):
-        # inputs, labels, W = gen.sample_single(it, cuda=torch.cuda.is_available(), is_training=False)
         loss_single, acc_single = test_mcd_single(gnn, gen, n_classes, it)
         loss_lst[it] = loss_single
         acc_lst[it] = acc_single
         torch.cuda.empty_cache()
-    print ('Testing results:')
-    print ('Avg test loss', np.mean(loss_lst))
-    print ('Avg test acc', np.mean(acc_lst))
-    print ('Std test acc', np.std(acc_lst))
-
+    print('Testing results:')
+    print('Avg test loss', np.mean(loss_lst))
+    print('Avg test acc', np.mean(acc_lst))
+    print('Std test acc', np.std(acc_lst))
 
 
 if __name__ == '__main__':
+    print('main file starts here')
 
-    print ('main file starts here')
-
-    # ## One fixed generator
+    # One fixed generator
     gen = Generator()
     # generator setup
     gen.N_train = args.N_train
@@ -258,68 +223,67 @@ if __name__ == '__main__':
     gen.generative_model = args.generative_model
     gen.n_classes = args.n_classes
 
-    torch.backends.cudnn.enabled=False
+    torch.backends.cudnn.enabled = False
 
-    if (args.mode == 'test'):
-        print ('In testing mode')
-        # filename = 'gnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + str(gen.N_test) + '_it' + str(args.iterations)
+    if args.mode == 'test':
+        print('In testing mode')
         filename = args.filename_existing_gnn
         path_plus_name = os.path.join(args.path_gnn, filename)
-        if ((filename != '') and (os.path.exists(path_plus_name))):
-            print ('Loading gnn ' + filename)
+        if (filename != '') and (os.path.exists(path_plus_name)):
+            print('Loading gnn ' + filename)
             gnn = torch.load(path_plus_name)
             if torch.cuda.is_available():
                 gnn.cuda()
         else:
-            print ('No such a gnn exists; creating a brand new one')
-            if (args.generative_model == 'SBM'):
+            print('No such a gnn exists; creating a brand new one')
+            if args.generative_model == 'SBM':
                 gnn = lGNN_multiclass(args.num_features, args.num_layers, args.J + 2)
-            elif (args.generative_model == 'SBM_multiclass'):
+            elif args.generative_model == 'SBM_multiclass':
                 gnn = lGNN_multiclass(args.num_features, args.num_layers, args.J + 2, n_classes=args.n_classes)
-            filename = 'lgnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + str(args.N_train) + '_num' + str(args.num_examples_train)
+            filename = 'lgnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + str(
+                args.N_train) + '_num' + str(args.num_examples_train)
             path_plus_name = os.path.join(args.path_gnn, filename)
             if torch.cuda.is_available():
                 gnn.cuda()
-            print ('Training begins')
-
-
-    elif (args.mode == 'train'):
+            print('Training begins')
+    elif args.mode == 'train':
         filename = args.filename_existing_gnn
         path_plus_name = os.path.join(args.path_gnn, filename)
-        if ((filename != '') and (os.path.exists(path_plus_name))):
-            print ('Loading gnn ' + filename)
+        if (filename != '') and (os.path.exists(path_plus_name)):
+            print('Loading gnn ' + filename)
             gnn = torch.load(path_plus_name)
             filename = filename + '_Ntr' + str(args.N_train) + '_num' + str(args.num_examples_train)
             path_plus_name = os.path.join(args.path_gnn, filename)
         else:
-            print ('No such a gnn exists; creating a brand new one')
-            filename = 'lgnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + str(args.N_train) + '_num' + str(args.num_examples_train)
+            print('No such a gnn exists; creating a brand new one')
+            filename = 'lgnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + \
+                       str(args.N_train) + '_num' + str(args.num_examples_train)
             path_plus_name = os.path.join(args.path_gnn, filename)
-            if (args.generative_model == 'SBM'):
+            if args.generative_model == 'SBM':
                 gnn = lGNN_multiclass(args.num_features, args.num_layers, args.J + 2)
-            elif (args.generative_model == 'SBM_multiclass'):
+            elif args.generative_model == 'SBM_multiclass':
                 gnn = lGNN_multiclass(args.num_features, args.num_layers, args.J + 2, n_classes=args.n_classes)
 
         if torch.cuda.is_available():
             gnn.cuda()
-        print ('Training begins')
-        if (args.generative_model == 'SBM'):
+        print('Training begins')
+        if args.generative_model == 'SBM':
             train(gnn, gen, 2)
-        elif (args.generative_model == 'SBM_multiclass'):
+        elif args.generative_model == 'SBM_multiclass':
             train(gnn, gen, args.n_classes)
-        print ('Saving gnn ' + filename)
+        print('Saving gnn ' + filename)
         if torch.cuda.is_available():
             torch.save(gnn.cpu(), path_plus_name)
             gnn.cuda()
         else:
             torch.save(gnn, path_plus_name)
 
-    print ('Testing the GNN:')
+    print('Testing the GNN:')
     if args.eval_vs_train:
-        print ('model status: eval')
+        print('model status: eval')
         gnn.eval()
     else:
-        print ('model status: train')
+        print('model status: train')
         gnn.train()
 
     test(gnn, gen, args.n_classes)
